092f6b4144222c67a8fb20aadf74a2a082584cde
Stian Langvann <stian.langvann@gmail.com>
2019-09-21 19:00:37 +0200

    Forcing myself to make a google search plugin

---
+++ bot/plugins/googlesearch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This plugin allows for a user to look up a movie through google.
+#for example; .gsearch "That movie with the yellow people in the dome and a spider pig"
+# search results will show movie name, ttimdbid and a short plot. 
